e52cec646c3094d6fda89112f9e9962e060ac437
김민오 <kimmo@gimmin-oui-MacBookAir.local>
2023-03-14 22:48:35 +0900

    first commit

---
+++ TEST2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+double factorial_iter(int n)
+{
+    int i ;
+    double result = 1 ;
+    for(i = 1; i <= n; i++)
+    {
+        result = result * i ;
+    }
+    return(result) ;
+}
+
+double factorial_rec(int n)
+{
+    if(n<=1) return (1);
+    
+    return (n * factorial_rec(n-1)) ;
+}
+
+int main(void) 
+{
+    int n = 20 ;
+    
+    double result_iter = factorial_iter(n) ;
+    double result_rec = factorial_rec(n) ;
+
+    printf("%f\n", result_iter) ;
+    printf("%f\n", result_rec) ;    
+}
